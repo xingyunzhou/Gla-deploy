0e49bf5f3e78453a8f5653a3a4662fa4a77938bc
xingyunzhou <34416577+xingyunzhou@users.noreply.github.com>
2025-12-14 21:22:32 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'saas.sin.fan';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'Galaxy';                  // 节点名称
 
@@ -625,4 +625,5 @@ app.get("/", async function(req, res) {
 });
 
 app.listen(PORT, () => console.log(`http server is running on port:${PORT}!`));
+
 
